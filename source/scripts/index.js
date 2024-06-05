3354846f9b8298f619dffc209f6dc757eb33e2de
Anna <108762848+Akyanna@users.noreply.github.com>
2024-06-05 12:41:40 +0300

    Module9-task3 (#10)
    * Изменяет меню
    
    * Добавляет реализацию JS
    
    * Вносит изменения в меню на моб.версии
    
    * Добавляет изменения в блок Hero
    
    * Исправлено выпадающее меню при JS
    
    * Вносит изменения по меню навигации
    
    * Реализация меню в потоке без JS
    
    * Вносит изменения в index и catalog

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+const Nav = document.querySelector('.navigation');
+const navToggle = document.querySelector('.navigation__toggle');
+
+Nav.classList.remove('navigation--nojs');
+
+Nav.classList.add('navigation--closed');
+
+navToggle.addEventListener('click', () => {
+  if (Nav.classList.contains('navigation--closed')) {
+    Nav.classList.remove('navigation--closed');
+    Nav.classList.add('navigation--opened');
+  } else {
+    Nav.classList.add('navigation--closed');
+    Nav.classList.remove('navigation--opened');
+  }
+});
